f58d58f44d58b7a62c8dad721134cf9d8f3f9d09
Wiktoria Szopa <98481689+wiktoria-szopa@users.noreply.github.com>
2023-03-26 21:00:55 +0200

    G UI (#4)
    * nie dziala ale poprawilam troche kod
    
    * nie dziala ale poprawilam troche kod
    
    * rewolucja przemyslowa
    
    * usuniete
    
    * zmergowane ale ma problemy
    
    * nwm
    
    * wyswietla sie x,y,v
    
    * wyswietla sie x,y,V, DODAJE SIE LADUNEK
    
    * dodane pop menu na chargu

--- src/chargesim/gui/CenterPanel.java
+++ src/chargesim/gui/CenterPanel.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 
 public class CenterPanel extends JPanel implements MouseListener, MouseMotionListener {
 
+    private static final int CHARGE_RADIUS = 20;
+
     public interface Listener {
         void cursorMoved(int x, int y);
 
@@ -20,7 +22,7 @@ public class CenterPanel extends JPanel implements MouseListener, MouseMotionLis
 
     public CenterPanel() {
         super();
-        setBackground(Color.PINK);
+        setBackground(Color.WHITE);
         addMouseListener(this);
         addMouseMotionListener(this);
     }
@@ -41,6 +43,16 @@ public class CenterPanel extends JPanel implements MouseListener, MouseMotionLis
 
     @Override
     public void mousePressed(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        if (SwingUtilities.isRightMouseButton(e)) {
+            for (Charge charge : charges) {
+                if (calculateDistance(x, y, charge.x, charge.y) < CHARGE_RADIUS) {
+                    showChargeMenu(x,y);
+                }
+            }
+
+        }
 
     }
 
@@ -65,14 +77,40 @@ public class CenterPanel extends JPanel implements MouseListener, MouseMotionLis
     }
     //endregion mouse listeners
 
-    public void addCharge(){
+    public void addCharge() {
         Charge charge = new Charge(this.getWidth() / 2, this.getHeight() / 2, 1);
         charges.add(charge);
-        this.getGraphics().drawImage(charge.positive.getImage(), charge.x-20, charge.y-20, 40, 40, null);
+        this.getGraphics()
+                .drawImage(
+                        charge.positive.getImage(),
+                        charge.x - CHARGE_RADIUS,
+                        charge.y - CHARGE_RADIUS,
+                        2 * CHARGE_RADIUS,
+                        2 * CHARGE_RADIUS,
+                        null
+                );
     }
 
     private double calculatePotential(int x, int y) {
         return 1000 * (1 / Math.sqrt(x * x + y * y));
+    }
+
+    private double calculateDistance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    private void showChargeMenu(int x, int y){
+        JPopupMenu chargeMenu = new JPopupMenu();
+        JMenuItem editItem = new JMenuItem("Edit");
+        JMenuItem copyItem = new JMenuItem("Copy");
+        JMenuItem deleteItem = new JMenuItem("Delete");
+
+        chargeMenu.add(copyItem);
+        chargeMenu.add(deleteItem);
+        chargeMenu.add(editItem);
+
+        chargeMenu.show(this, x, y);
+
     }
 
 }
