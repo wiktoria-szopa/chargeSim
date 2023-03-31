09771b4f1d2a44bd78a2b2f5aa03cfdc7d937863
rekwizytor <adamrek.st9@gmail.com>
2023-03-31 23:15:04 +0200

    usuniete zbedne rzeczy w charge i usuniety folder images

--- src/chargesim/Charge.java
+++ src/chargesim/Charge.java
@@ -8,10 +8,7 @@ public class Charge {
     public int x;
     public int y;
 
-    public ImageIcon positive = new ImageIcon("src/chargesim/dodatni.png");
-
-
-    public Charge(int x, int y, double value) { //tutaj z argumentu usuniety Jpanel na ktorym to ma sie tworzyc
+    public Charge(int x, int y, double value) {
         this.x = x;
         this.y = y;
         this.value = value;
